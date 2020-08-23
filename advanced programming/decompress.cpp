6830c3112a9e38465d24a294f690bbe7d813af62
tshuwang <562658249@qq.com>
2020-08-23 23:03:12 +0800

    finish

--- advanced programming/decompress.cpp
+++ advanced programming/decompress.cpp
@@ -91,10 +91,102 @@ string decode(string str)
 	return result; 
 } 
 
+
+string decompress(string str)
+{
+	stack<int> sint;
+	stack<char> schr;
+	string res="";
+	string temp="";
+	for (int i = 0; i <str.length(); i++)
+	{
+		//数字
+		int count=0;
+		if(str[i]>='0'&&str[i]<='9')
+		{
+			while (str[i]>='0'&&str[i]<='9')
+			{
+				count=count*10+str[i]-'0';
+				i++;//移动到下一个
+			}
+			sint.push(count);
+			i--;
+		}
+		
+		// [ 字符
+		else if(str[i]=='[')
+		{
+			if(str[i-1]>='0'&&str[i-1]<='9')//前面是数字
+			{
+				schr.push(str[i]);
+			}
+
+			else
+			{
+				schr.push(str[i]);
+				sint.push(1);
+			}		
+
+		}
+
+		// ] 字符
+		else if(str[i]==']')
+		{
+			temp="";
+			count=0;
+
+			if(!sint.empty())
+			{
+				count=sint.top();
+				sint.pop();
+			}
+
+			while(!schr.empty()&&schr.top()!='[')
+			{
+				temp=schr.top()+temp;
+				schr.pop();
+			}
+
+			while (!schr.empty()&&schr.top()=='[')
+			{
+				schr.pop();
+			}
+
+			for(int j=0;j<count;j++)
+			{
+				res=res+temp;
+			}
+			
+			for(int j=0;j<res.length();j++)
+			{
+				schr.push(res[j]);
+			}
+
+			res="";
+		}
+		
+		//字母
+		else
+		{
+			schr.push(str[i]);
+		}		
+	}
+	
+	
+	while(!schr.empty())
+	{
+		res=schr.top()+res;
+		schr.pop();
+	}
+
+	return res;
+}
+
 // Driven Program 
 int main() 
 { 
 	string str = "3[b2[ca]]"; 
-	cout << decode(str) << endl; 
+	cout << decode(str) << endl;
+	cout << decompress(str)<< endl; 
 	return 0; 
 } 
